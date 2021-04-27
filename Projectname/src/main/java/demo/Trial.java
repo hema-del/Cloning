ebd63f655319de36e6b7e718c826b3752a38561a
karth <karth@LAPTOP-7076T1TI>
2021-04-27 09:18:24 +0530

    Initial commit

---
+++ Projectname/src/main/java/demo/Trial.java
@@ -0,0 +1,19 @@
+package demo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Trial{
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\karth\\node_modules\\geckodriver\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.google.com");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://www.omayo.blogspot.com");
+	}
+}
+
+
+
